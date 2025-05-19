aeb593c0a7435c40274c345356e21f1ea0f4c536
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-05-20 07:42:09 +0900

    update new pdf visualization stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ai-pdf-writer
 
+Install tesseract on linux using the following command:
+```
+sudo apt -y install tesseract-ocr tesseract-ocr-jpn libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
+```
+
+Then, head over to https://github.com/tesseract-ocr/tessdata/tags and download the latest version of tessdata and put it in the `$HOME/.tessdata`. Set `TESSDATA_PREFIX` to `$HOME/.tessdata` in your `.bashrc` file.
--- pdf_viz.py
+++ pdf_viz.py
@@ -1,10 +1,21 @@
-from transformers import LayoutLMv3Processor, LayoutLMv3FeatureExtractor, LayoutLMv3FastTokenizer
-from transformers.model.fnet.modeling_fnet import apply_chunking_to_forward
+from transformers import LayoutLMv3Processor, LayoutLMv3FeatureExtractor, LayoutLMv3TokenizerFast
 from PIL import Image
 from pdf2image import convert_from_path
 
-feature_extractor = LayoutLMv3FeatureExtractor(apply_ocr=True, ocr_lang="en")
-tokenizer = LayoutLMv3FastTokenizer.from_pretrained("microsoft/layoutlmv3-base")
+feature_extractor = LayoutLMv3FeatureExtractor(apply_ocr=True, ocr_lang="eng")
+tokenizer = LayoutLMv3TokenizerFast.from_pretrained("microsoft/layoutlmv3-base")
 processor = LayoutLMv3Processor(feature_extractor, tokenizer)
 pdf_images = convert_from_path("letter_of_guarantee.pdf")
+
+for image in pdf_images:
+    image.save("le.png")
+    encoding = processor(
+        image,
+        max_length=1024,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt"
+    )
+
+    print(encoding.keys())
 
