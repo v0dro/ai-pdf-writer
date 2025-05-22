01a8ae44a5579cc971117222a65272e163c66753
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-05-22 23:57:44 +0900

    update pdf viz

---
+++ donut.py
@@ -0,0 +1,36 @@
+import re
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+from datasets import load_dataset
+from PIL import Image
+import torch
+
+processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+device = "cpu"
+model.to(device)  # doctest: +IGNORE_RESULT
+
+# load document image
+image = Image.open("processed_output.png").convert("RGB")
+
+# prepare decoder inputs
+task_prompt = "<s_docvqa><s_question>what is the content of the document?<s_answer>"
+decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+pixel_values = processor(image, return_tensors="pt").pixel_values
+
+outputs = model.generate(
+    pixel_values.to(device),
+    decoder_input_ids=decoder_input_ids.to(device),
+    max_length=model.decoder.config.max_position_embeddings,
+    pad_token_id=processor.tokenizer.pad_token_id,
+    eos_token_id=processor.tokenizer.eos_token_id,
+    use_cache=True,
+    bad_words_ids=[[processor.tokenizer.unk_token_id]],
+    return_dict_in_generate=True,
+)
+
+sequence = processor.batch_decode(outputs.sequences)[0]
+sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+print(processor.token2json(sequence))
