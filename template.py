b4e51e0b469b1126c3836a549dc79bef16f4f3c9
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-06-02 01:16:38 +0900

    Working for finding form fields

--- template.py
+++ template.py
@@ -8,31 +8,35 @@ template2 = cv2.imread("dots2.png", cv2.IMREAD_GRAYSCALE)
 
 def combine_rectangles(loc, h, w):
     combined = []
+    prev_x = 0
+    x =0
     in_rect = False
-    width = 0
-    y = None
-    x = None
     for pt in zip(*loc):
-        # print("x:", x, " y: ", y, " width: ", width, " w: ", w)
         if not in_rect:
             start_y = pt[0]
             start_x = pt[1]
-            width = 0
+            prev_x = start_x
+            width = 8
             in_rect = True
+            continue
+        
+        y = pt[0]
+        x = pt[1]
+
+        if y != start_y:
+            combined.append((start_y, start_x, h, width))
+            in_rect = False
+            continue
+
+        if x <= start_x + width:
+            print("x:", x, "y:", y, "start_x:", start_x, "width:", width)
+            width += (x - prev_x)
         else:
-            prev_y = y
-            prev_x = x
-            y = pt[0]
-            x = pt[1]
+            combined.append((start_y, start_x, h, width))
+            in_rect = False
+        prev_x = x
 
-
-            if x <= start_x + width:
-                print("start_y:", start_y, "start_x:", start_x, "y:", y, "x:", x, "width:", width, "w:", w, "start_x + width:", start_x + width, " x-start_x:", x-start_x)
-
-                width += (x - start_x)
-            else:
-                in_rect = False
-                combined.append((start_y, start_x, h, width + w))
+    combined.append((start_y, start_x, h, width))
 
     # Return a list with tuples (x, y, w, h) for each combined rectangle.
     return combined
@@ -43,6 +47,7 @@ h, w = template.shape[::-1]
 # The x-coordinates increase to the right, and the y-coordinates increase downwards.
 res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
 threshold = 0.9
+#1115-1027
 
 # Therefore, the locations here are a 2D array where the rows indicate the
 # y-coordinates and the columns indicate the x-coordinates.
@@ -50,7 +55,7 @@ loc = np.where(res >= threshold)
 # for pt in zip(*loc):  # Switch columns and rows
 #     print("x:", pt[0], "y:", pt[1], "w:", w, "h:", h)
 #     cv2.rectangle(img, (pt[1], pt[0]), (pt[1] + h, pt[0] + w), (0, 255, 0), 2)
-
+#cv2.rectangle(img, (507, 1317), (507 +550, 1317 + h), (0, 255, 0), 2)
 combined_rectangles = combine_rectangles(loc, h, w)
 print(combined_rectangles)
 
@@ -58,13 +63,21 @@ for pt in combined_rectangles:  # Switch columns and rows
     # print("x:", pt[0], "y:", pt[1], "w:", w, "h:", h)
     cv2.rectangle(img, (pt[1], pt[0]), (pt[1] + pt[3], pt[0] + pt[2]), (0, 255, 0), 2)
 
-# w2, h2 = template2.shape[::-1]
+h2, w2 = template2.shape[::-1]
 
-# #print("next template")
-# res2 = cv2.matchTemplate(img, template2, cv2.TM_CCOEFF_NORMED)
-# loc2 = np.where(res2 >= threshold)
+print("next template")
+res2 = cv2.matchTemplate(img, template2, cv2.TM_CCOEFF_NORMED)
+loc2 = np.where(res2 >= threshold)
 # for pt in zip(*loc2[::-1]):  # Switch columns and rows
 #     #print("x:", pt[0], "y:", pt[1], "w:", w2, "h:", h2)
 #     cv2.rectangle(img, pt, (pt[0] + w2, pt[1] + h2), (0, 255, 0), 2)
 
-cv2.imwrite("result.png", img)
+combined_rectangles = combine_rectangles(loc2, h, w2)
+print(combined_rectangles)
+
+for pt in combined_rectangles:  # Switch columns and rows
+    # print("x:", pt[0], "y:", pt[1], "w:", w, "h:", h)
+    cv2.rectangle(img, (pt[1], pt[0]), (pt[1] + pt[3], pt[0] + pt[2]), (0, 255, 0), 2)
+
+
+cv2.imwrite("result1.png", img)
